58c544e1eaf95569e245e4f505c2b6e64ab3d3f6
George King <george@betterde.com>
2026-04-18 12:29:37 +0800

    chore: replace open source license link

--- voyager_common.c
+++ voyager_common.c
@@ -5,7 +5,7 @@
 | This source file is subject to the MIT license,
 | that is bundled with this package in the file LICENSE, and is        |
 | available through the world-wide-web at the following url:           |
-| http://www.opensource.org/licenses/MIT
+| https://opensource.org/license/MIT
 +----------------------------------------------------------------------+
 | Author: George King <george@betterde.com>                            |
 +----------------------------------------------------------------------+
